8655e15cdb15eafbf8b19b12992cce40ccb2fec2
Felix-DataAnalyst <felixonome69@gmail.com>
2024-08-20 05:06:23 +0100

    My Python Journey....from crawling, walking to running

--- day 001 project.py
+++ day 001 project.py
@@ -4,3 +4,9 @@ print("Welcome to the Band Name Generator")
 username1 = input("What's the name of the city you grew up in?\n")
 petname = input("What's your pet's name?\n")
 print("Your Band name could be "+username1.title() +" "+petname.title())
+
+#
+# print("Welcome to the Band Name Generator")
+# username1 = input("What's the name of the city you grew up in?\n")
+# petname = input("What's your pet's name?\n")
+# print("Your Band name could be "+username1.title() +" "+petname.title())
