4c6f89ac600dc0169b7bed86b2152b48e3cd090e
Felix-DataAnalyst <felixonome69@gmail.com>
2024-08-20 02:50:18 +0100

    My Python Journey....from crawling, walking to running

---
+++ DAY 001.py
@@ -0,0 +1,24 @@
+#Day 1 of 100 Days of Coding with Python
+
+print("Welcome to My Python Journey....from crawling, walking to running")
+
+#creating a new line
+print("Hello World\nHello World\nHello World")
+
+#String Concatenation
+print("Hello"+ " "+"Angela")
+
+#INPUT Function-Getting response from users
+print("Hello " + input("What is your Name?") + "!")
+
+#Variables
+name = input("What is your Name?") #Variable is used to store values in container for later use
+
+print(len(name)) #How many characters are in the name inputed above
+print(len(input("What is your Name?")))
+
+username = input("What is your Name?")
+length = len(username)
+print(length)
+
+print("End of day one practice... heading to project")
---
+++ day oo1 project.py
@@ -0,0 +1,6 @@
+
+# PROJECT FOR DAY ONE - Band Name Generator
+print("Welcome to the Band Name Generator")
+username1 = input("What's the name of the city you grew up in?\n")
+petname = input("What's your pet's name?\n")
+print("Your Band name could be "+username1.title() +" "+petname.title())
